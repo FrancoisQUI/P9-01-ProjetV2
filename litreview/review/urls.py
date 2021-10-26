ade3b26117bda36ccd3bdf80aeca9fa6c3047e97
Francois QUIVRON <fquivron@gmail.com>
2021-10-26 17:13:27 +0200

    Add review detail view

--- litreview/review/urls.py
+++ litreview/review/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from review.views import ReviewListView
+from review.views import ReviewListView, ReviewDetailView
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='review_list'),
+    path('<int:pk>', ReviewDetailView.as_view(), name='review_detail'),
+
 ]
